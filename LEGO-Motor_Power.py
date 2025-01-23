2b1593ad178311659b5f4ea58756d284e03f2ec5
simrankaware <simrankaware@gmail.com>
2025-01-23 17:12:24 +0000

    Create program to drive in straight line

--- LEGO-Motor_Power.py
+++ LEGO-Motor_Power.py
@@ -1,3 +1,5 @@
+
+
 #!/usr/bin/env python
 #
 # https://www.dexterindustries.com/BrickPi/
@@ -20,7 +22,6 @@ import time     # import the time library for the sleep function
 import brickpi3 # import the BrickPi3 drivers
 
 BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
-
 try:
     try:
         BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B)) # reset encoder B
