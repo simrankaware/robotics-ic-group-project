934b39f6325d5312fd87357e921709a6c34bf59d
AaronTomThomas <at2222@ic.ac.uk>
2025-01-27 17:20:35 +0000

    attempt calc

--- straight_line.py
+++ straight_line.py
@@ -6,22 +6,62 @@ import brickpi3 # import the BrickPi3 drivers
 
 BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
 
+LEFT_MOTOR = BP.PORT_B
+RIGHT_MOTOR = BP.PORT_C
+
+#Parameters
+BASE_POWER = 15
+CORRECTION_GAIN = 1.5
+
+
+
+
 try:
-    begin = time.time()
-    print(begin)
-    while time.time() - begin < 5:
-        time.sleep(1)
-        print(time.time() - begin)
-    try:
-        target = BP.set_motor_power(BP.PORT_C, -15)
-    except IOError as error:
-        print(error)
-    BP.set_motor_position(BP.PORT_B, target)
-    begin = time.time()
-    while time.time() - begin < 15:
-        time.sleep(1)
-        print(time.time() - begin)
-    BP.reset_all()
+    # begin = time.time()
+    # print(begin)
+    # while time.time() - begin < 5:
+    #     time.sleep(1)
+    #     print(time.time() - begin)
+    # try:
+    #     target = BP.set_motor_power(BP.PORT_C, -15)
+    # except IOError as error:
+    #     print(error)
+    # BP.set_motor_position(BP.PORT_B, target)
+    # begin = time.time()
+    # while time.time() - begin < 15:
+    #     time.sleep(1)
+    #     print(time.time() - begin)
+    # BP.reset_all()
+    
+    BP.offset_motor_encoder(LEFT_MOTOR, BP.get_motor_encoder(LEFT_MOTOR))
+    BP.offset_motor_encoder(RIGHT_MOTOR, BP.get_motor_encoder(RIGHT_MOTOR))
+
+    start_time = time.time()
+    print(start_time)
+
+    duration = 200
+    while time.time() - start_time < duration:
+        left_encoder = BP.get_motor_encoder(LEFT_MOTOR)
+        right_encoder = BP.get_motor_encoder(RIGHT_MOTOR)
+
+        # calculating error diff
+        error = left_encoder - right_encoder
+        print("Error: %6d" % error)
+        # calculating correction 
+        correction = error * CORRECTION_GAIN
+
+        left_power = BASE_POWER - correction
+        right_power = BASE_POWER + correction
+
+        BP.set_motor_power(LEFT_MOTOR, left_power)
+        BP.set_motor_power(RIGHT_MOTOR, right_power)
+
+        print("Left encoder: %6d  Right encoder: %6d" % (left_encoder, right_encoder))
+
+        time.sleep(0.05)
+
+    BP.set_motor_power(LEFT_MOTOR, 0)
+    BP.set_motor_power(RIGHT_MOTOR, 0)
 except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
     BP.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the >
 
