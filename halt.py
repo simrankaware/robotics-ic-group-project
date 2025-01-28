bdc672f20c976aef787348c5737a027a8ddfa37b
simrankaware <simrankaware@gmail.com>
2025-01-28 15:46:29 +0000

    add halt

---
+++ halt.py
@@ -0,0 +1,9 @@
+from __future__ import print_function # use python 3 syntax but make it compatible with python 2
+from __future__ import division       #                           ''
+
+import time     # import the time library for the sleep function
+import brickpi3 # import the BrickPi3 drivers
+
+BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
+
+BP.reset_all()
