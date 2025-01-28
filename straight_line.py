073f5f054d4bdfbdf9df899ee8dc2d0456ed3814
Simran Kaware <simrankaware@gmail.com>
2025-01-28 16:02:31 +0000

    calibrating rotation pt2

--- straight_line.py
+++ straight_line.py
@@ -55,8 +55,8 @@ def rotate(degrees):
     target_degrees = degrees * (360 / 90)  # Adjust this factor based on your robot's configuration
     position_l = BP.get_motor_encoder(LEFT_MOTOR)
     position_r = BP.get_motor_encoder(RIGHT_MOTOR)
-    BP.set_motor_position(LEFT_MOTOR, position_l+200)
-    BP.set_motor_position(RIGHT_MOTOR, position_r-200)
+    BP.set_motor_position(LEFT_MOTOR, position_l+180)
+    BP.set_motor_position(RIGHT_MOTOR, position_r-180)
     # print( "Position L: ", position_l, "Position R: ", position_r)
     # while True:
     #     left_encoder = BP.get_motor_encoder(LEFT_MOTOR)
