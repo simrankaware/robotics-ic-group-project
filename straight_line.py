84572aef4d6035da2506d576f7391d0cc435cb66
Simran Kaware <simrankaware@gmail.com>
2025-01-28 15:43:51 +0000

    playing around iwth rotatew

--- straight_line.py
+++ straight_line.py
@@ -56,7 +56,7 @@ def rotate(degrees):
     _, _, _, position_l = BP.get_motor_encoder(LEFT_MOTOR)
     _, _, _, position_r = BP.get_motor_encoder(RIGHT_MOTOR)
     BP.set_motor_position(LEFT_MOTOR, position_l-225)
-    BP.set_motor_position(RIGHT_MOTOR, position_r -225)
+    BP.set_motor_position(RIGHT_MOTOR, position_r +225)
     print( "Position L: ", position_l, "Position R: ", position_r)
     # while True:
     #     left_encoder = BP.get_motor_encoder(LEFT_MOTOR)
