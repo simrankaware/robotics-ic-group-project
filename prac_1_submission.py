a76a286c6900fb54293e55a4d8364adb3dd922f9
Simran Kaware <simrankaware@gmail.com>
2025-02-04 16:29:34 +0000

    Setup square for week 3

--- prac_1_submission.py
+++ prac_1_submission.py
@@ -12,7 +12,7 @@ WHEEL_RADIUS = 3.05
 AXLE_RADIUS = 6.5
 WHEEL_ROTATION_FOR_90_DEGREES = ((AXLE_RADIUS * pi) / 2) / (2 * pi * WHEEL_RADIUS) * 360
 
-# Parameters
+#Parameters
 BASE_POWER = -15
 kp = 1.5
 
@@ -20,37 +20,6 @@ TIME_DELAY = 300 / (120 * pi)
 
 ANGLE_CALIBRATION = 39.75
 
-DISTANCE = 40 # in cm
-ANG_VELOCITY = -120
-
-
-
-def drive_straight_for_time(duration):
-    BP.offset_motor_encoder(LEFT_MOTOR, BP.get_motor_encoder(LEFT_MOTOR))
-    BP.offset_motor_encoder(RIGHT_MOTOR, BP.get_motor_encoder(RIGHT_MOTOR))
-
-    start_time = time.time()
-    print(start_time)
-
-    while time.time() - start_time < duration:
-        left_encoder = BP.get_motor_encoder(LEFT_MOTOR)
-        right_encoder = BP.get_motor_encoder(RIGHT_MOTOR)
-
-        # calculating error diff
-        error = left_encoder - right_encoder
-        print("Error: %6d" % error)
-        # calculating correction 
-        correction = error * kp
-
-        left_power = BASE_POWER - correction
-        right_power = BASE_POWER + correction
-
-        BP.set_motor_power(LEFT_MOTOR, left_power)
-        BP.set_motor_power(RIGHT_MOTOR, right_power)
-
-        print("Left encoder: %6d  Right encoder: %6d" % (left_encoder, right_encoder))
-
-        time.sleep(0.05) 
 
 def drive_straight_for_distance(distance, speed=-20): # distance in cm
     BP.offset_motor_encoder(LEFT_MOTOR, BP.get_motor_encoder(LEFT_MOTOR))
@@ -75,8 +44,8 @@ def rotate(degrees, speed=35):  # Add a speed parameter (default: 50 dps)
 
     target_degrees = degrees # Adjust factor based on calibration
 
-    BP.set_motor_dps(LEFT_MOTOR, -speed)   # Set slow speed
-    BP.set_motor_dps(RIGHT_MOTOR, speed) # Opposite direction for rotation
+    BP.set_motor_dps(LEFT_MOTOR, speed)   # Set slow speed
+    BP.set_motor_dps(RIGHT_MOTOR, -speed) # Opposite direction for rotation
 
     sleep_time = abs(target_degrees / speed)
     sleep_time = sleep_time - TIME_DELAY if TIME_DELAY < sleep_time else 0
@@ -85,9 +54,17 @@ def rotate(degrees, speed=35):  # Add a speed parameter (default: 50 dps)
     print(f"Done with rotate at\n L: {BP.get_motor_encoder(LEFT_MOTOR)}\n  R: {BP.get_motor_encoder(RIGHT_MOTOR)}")
 
 
-
-
-if __name__ == "__main__":
-  for _ in range(4):
-    drive_straight_for_distance(DISTANCE, ANG_VELOCITY)
+try:
+    drive_straight_for_distance(40, -120)
     rotate(WHEEL_ROTATION_FOR_90_DEGREES)
+    drive_straight_for_distance(40, -120)
+    rotate(WHEEL_ROTATION_FOR_90_DEGREES)
+    drive_straight_for_distance(40, -120)
+    rotate(WHEEL_ROTATION_FOR_90_DEGREES)
+    drive_straight_for_distance(40, -120)
+    rotate(WHEEL_ROTATION_FOR_90_DEGREES)
+    BP.reset_all()
+except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
+    BP.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the >
+
+
